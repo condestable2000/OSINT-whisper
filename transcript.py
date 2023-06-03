2d6bba0fec299ce66d401b3d67e305f534111aaa
condestable2000 <condestable2000@users.noreply.github.com>
2023-06-04 01:01:31 +0200

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -1,9 +1,7 @@
 import whisper
 import os
 
-def transcribe(file_input):
-    # load the model
-    model = whisper.load_model("base")
+def transcribe(file_input, model):
 
     # load audio and pad/trim it to fit 30 seconds
     audio = whisper.load_audio(file_input)
@@ -18,6 +16,11 @@ def transcribe(file_input):
     no_ext_file_input = name_file_input.split(".")
     name_file_output = no_ext_file_input[0] + ".txt"
 
+    print('route: ',route_file_input[:-1])
+    print('name_file_input: ',name_file_input)
+    print('no_ext_file_input: ',no_ext_file_input)
+    print('name_file_output: ',name_file_output)
+    
     # save the transcription to a file
     with open(name_file_output, "w") as f:
         # detect the spoken language
@@ -30,15 +33,18 @@ def transcribe(file_input):
 
         # print the recognized text
         f.write(result.text)   
-    
+    print(result.text  + "\n\n\n")
 
-def files_manager(rute):
-    files = os.listdir(rute)
+def files_manager(route, model):
+    files = os.listdir(route)
     for file in files:
-        transcribe(file)
+        print("file: ", route + "/" + file )
+        transcribe(route + "/" + file, model)
 
 if __name__ == "__main__":
     # get inut parameters
-    rute = input("Enter the rute of the audio files: ")
-    files_manager(rute)
-    print("The transcription is done")
+    route = input("Enter the rute of the audio files: ")
+    # load the model
+    model = whisper.load_model("medium") # "large" | "medium" | "small" | "base" | "tiny"
+    files_manager(route, model)
+    print("The transcription is done!!!")
